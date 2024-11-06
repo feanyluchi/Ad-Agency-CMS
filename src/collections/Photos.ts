db8b19a33c84246b240e7ad103279e23b9ef6c6a
feanyluch <ifeanyioluchi7@gmail.com>
2024-11-06 16:33:15 +0100

    TES-3435: Focal point

--- src/collections/Photos.ts
+++ src/collections/Photos.ts
@@ -1,10 +1,40 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import type { CollectionConfig } from 'payload'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const Photos: CollectionConfig = {
   slug: 'photos',
   labels: {
     singular: "Photo's",
     plural: "Photo's",
+  },
+  upload: {
+    staticDir: path.resolve(__dirname, '../../../media'),
+    imageSizes: [
+      {
+        name: 'mobile',
+        width: 480,
+        height: undefined,
+        position: 'centre',
+      },
+      {
+        name: 'tablet',
+        width: 768,
+        height: undefined,
+        position: 'centre',
+      },
+      {
+        name: 'desktop',
+        width: 1920,
+        height: undefined,
+        position: 'centre',
+      },
+    ],
+    crop: true,
+    focalPoint: true,
   },
   access: {
     read: () => true,
@@ -16,5 +46,4 @@ export const Photos: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
 }
