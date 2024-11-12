40449e173166b0705886f380e3f0be486630285f
feanyluch <ifeanyioluchi7@gmail.com>
2024-11-12 21:17:24 +0100

    TES 3523: About us section in beta template

---
+++ src/blocks/beta/AboutUs/index.ts
@@ -0,0 +1,79 @@
+import link from "@/fields/link";
+import { Block } from "payload";
+
+const AboutUsBlock: Block = {
+    slug: 'about-us',
+    labels: {
+      singular: 'About Us Section',
+      plural: 'About Us Sections',
+    },
+    fields: [
+      {
+        name: 'subtitle',
+        type: 'text',
+        label: 'Subtitle',
+        required: true,
+        localized: true
+      },
+      {
+        name: 'title',
+        type: 'text',
+        label: 'Title',
+        required: true,
+        localized: true
+      },
+      {
+        name: 'experienceImage',
+        type: 'upload',
+        relationTo: 'photos',
+        label: 'Experience Image',
+        required: true,
+      },
+      {
+        name: 'nightImage',
+        type: 'upload',
+        relationTo: 'photos',
+        label: 'Night Image',
+        required: true,
+      },
+      {
+        name: 'experience',
+        type: 'text',
+        label: 'Experience Text',
+        required: true,
+        localized: true,
+        defaultValue: '20 Years of Experience',
+      },
+      {
+        name: 'description',
+        type: 'richText',
+        label: 'Description',
+        required: true,
+      },
+      {
+        name: 'testimonialsImage',
+        type: 'array',
+        label: 'Testimonial Images',
+        minRows: 1,
+        maxRows: 3,
+        fields: [
+          {
+            type: 'upload',
+            name: 'image',
+            relationTo: 'photos',
+            label: 'Image',
+          },
+        ],
+      },
+      {
+        name: 'testimonialsCount',
+        type: 'text',
+        label: 'Testimonials Count',
+        defaultValue: '20k+ Satisfied Homeowners',
+      },
+      link(),
+    ],
+  };
+  
+  export default AboutUsBlock;
+  
