c949380a368dd9891280928b79dcf9c21fb39839
feanyluch <ifeanyioluchi7@gmail.com>
2025-04-14 16:35:41 +0100

    Fix: Deployment issue by changing config name

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,7 +2,7 @@ import { headers as getHeaders } from 'next/headers.js'
 import { getPayload } from 'payload'
 import React from 'react'
 
-import config from '@/ada.config'
+import config from '@/payload.config'
 import './styles.css'
 
 export default async function HomePage() {
